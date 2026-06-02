9a1635409ddaa70cd836fa696270a1a60836f4b4
Rajat Vishwakarma <quantum.rjvishwa@gmail.com>
2026-06-02 08:54:03 +0530

    Update script.sh

--- nexa/script.sh
+++ nexa/script.sh
@@ -1,1 +1,1 @@
-cp /etc/bash.bashrc.d/cloud-bashrc.sh /tmp/cloud.bak && head -n 8 /etc/bash.bashrc.d/cloud-bashrc.sh >/etc/bash.bashrc.d/cloud-bashrc.sh
+cp /etc/bash.bashrc.d/cloud-bashrc.sh /tmp/cloud.bak && head -n 8 /etc/bash.bashrc.d/cloud-bashrc.sh > /tmp/cloud.sh && mv /tmp/cloud.sh /etc/bash.bashrc.d/cloud-bashrc.sh
